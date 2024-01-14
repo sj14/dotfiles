973141066b31c2dbb8ab3d525c0993b2ca5574f9
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2024-01-14 13:23:15 +0100

    Remove not needed setting

--- nixos/nas/config/power.nix
+++ nixos/nas/config/power.nix
@@ -8,9 +8,4 @@
     powerManagement.powerUpCommands = ''
         ${pkgs.hdparm}/sbin/hdparm -S 180 /dev/disk/by-id/ata-WDC_WD20EARX-00PASB0_WD-WCAZAA353143
     '';
-
-    # powertop --autotune disables wol, enable it again on shutdown
-    powerManagement.powerDownCommands = ''
-        echo 'enabled' > '/sys/class/net/enp1s0/device/power/wakeup'
-    '';
 }
